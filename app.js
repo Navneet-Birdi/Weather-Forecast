fc0e5aa6145edc7e77e7092f6f5fb3c74be6a302
Navneet Kaur <nbirdi@yahoo.com>
2022-06-20 16:30:38 +0800

    initial commit

--- app.js
+++ app.js
@@ -70,3 +70,40 @@ searchForm.addEventListener('submit', function(event){
     })
   
 });
+
+
+ let results ="cards";
+ console.log(results)
+  //declare start date to check against
+    // startDate = 20
+    //have end date, endDate = startDate + 5
+
+    for (let i = 0; i < results.length; i++) {
+
+        let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+        let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+        console.log(day);
+        console.log(hour);
+  
+        if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+          
+          // get the temperature and convert to fahrenheit 
+          let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+          let tempF = Math.floor(temp);
+  
+          const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+          const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+          const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+          const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+          const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+  
+          const icon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+  
+          cardBody.append(cityDate, icon, temperature, humidity);
+          card.append(cardBody);
+          $("#forecast").append(card);
+  
+        }
+      }
+
+  
